47a507bb98234f016efa83ab97bba3a6b10404d9
helentangg <83192051+helentangg@users.noreply.github.com>
2023-06-11 17:56:30 -0400

    looped

--- src/Utility.js
+++ src/Utility.js
@@ -1,25 +1,42 @@
-/*
-getDros function returns the drosophila in the matchup
-returns the drosophila objects as a list
-*/
+// export const getDros = async () => {
+//   let dros1n = Math.round(Math.random() * 99),
+//     dros2n = Math.round(Math.random() * 99);
+//   while (dros1n == dros2n) dros2n = Math.round(Math.random() * 99);
+//   const dros1 = await fetch(
+//     "https://ipfs.io/ipfs/QmcppupRBTq8Q1xMJ14d8vR1GCQUdWr1cLN21vNJC8jwKr/" +
+//       dros1n
+//   );
+//   const dros2 = await fetch(
+//     "https://ipfs.io/ipfs/QmcppupRBTq8Q1xMJ14d8vR1GCQUdWr1cLN21vNJC8jwKr/" +
+//       dros2n
+//   );
+//   return [await dros1.json(), await dros2.json()];
+// };
+
 export const getDros = async () => {
-  return [];
+  const dros = [];
+  for (let i = 0; i < 100; i++) {
+    const dros1 = await fetch(
+      "https://ipfs.io/ipfs/QmcppupRBTq8Q1xMJ14d8vR1GCQUdWr1cLN21vNJC8jwKr/" +
+        i
+    );
+    dros.push(await dros1.json());
+
+  }
+    return dros;
 };
 
-/*
-runFight function simulates the fight
-requires n-dimensional list of drosophila objects
-returns the winning drosophila
-*/
-export const runFight = (drosList) => {
-  return {};
+export const getOdds = (dros1, dros2) => {
+  return [
+    reduce(dros1?.score, dros2?.score)[0],
+    reduce(dros1?.score, dros2?.score)[1],
+  ];
 };
 
-/*
-getOdds function returns the odds of the matchup as a tuple
-requires n-dimensional list of drosophila objects and mode parameter
-returns the odds as a list
-*/
-export const getOdds = (drosList, mode) => {
-  return [];
-};
+function reduce(numerator, denominator) {
+  var gcd = function gcd(a, b) {
+    return b ? gcd(b, a % b) : a;
+  };
+  gcd = gcd(numerator, denominator);
+  return [numerator / gcd, denominator / gcd];
+}
